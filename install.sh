002fca891a836fbc7e1856b833b263c4461270a2
lostbean <talktoedgar@gmail.com>
2020-10-04 04:58:09 +0200

    do not link folder

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ mkdir -p $HOME/.config/plasma-workspace/env/
 ln -i -s $(pwd)/config/plasma-workspace/env/set_window_manager.sh $HOME/.config/plasma-workspace/env/set_window_manager.sh
 
 mkdir -p $HOME/.xfunky
-ln -i -s $(pwd)/statusbar $HOME/.xfunky/statusbar
+mkdir -p $HOME/.xfunky/statusbar
+ln -i -s $(pwd)/statusbar/* $HOME/.xfunky/statusbar
 
 cd xmonad
 stack install
